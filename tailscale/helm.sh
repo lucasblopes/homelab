d2775c9b7b24834a632d0c33314e8d9d520683fe
Lucas Lopes <lucas.blopes3@gmail.com>
2025-09-28 19:45:06 -0300

    Add tailscale operator

---
+++ tailscale/helm.sh
@@ -0,0 +1,16 @@
+# docs:
+# https://tailscale.com/kb/1236/kubernetes-operator#setup
+
+helm repo add tailscale https://pkgs.tailscale.com/helmcharts
+
+helm repo update
+
+helm upgrade \
+    --install \
+    tailscale-operator \
+    tailscale/tailscale-operator \
+    --namespace=tailscale \
+    --create-namespace \
+    --set-string oauth.clientId="<OAauth client ID>" \
+    --set-string oauth.clientSecret="<OAuth client secret>" \
+    --wait
